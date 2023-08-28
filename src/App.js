06ac365cc0ba7a35f30943a618f8b8fc7c4bd2a8
DarshilModi29 <132668473+DarshilModi29@users.noreply.github.com>
2023-08-28 17:55:54 +0530

    Text-Utils

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+// import About from './components/About';
+// import { Routes, Route } from "react-router-dom"
 
 function App() {
+  const [Mode, setMode] = useState('light'); //Whether dark mode is unable or not
+  const [btnClass, setbtnClass] = useState('text-dark');
+  const [btnText, setBtnText] = useState('Enable Dark Mode');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
+
+  const toggleMode = () => {
+    if(Mode === 'light'){
+      setMode('dark');
+      setbtnClass('text-light');
+      setBtnText('Disable Dark Mode');
+      document.body.style.backgroundColor = "black";
+      showAlert("Dark Mode has been enabled", "success");
+    }
+    else
+    {
+      setMode('light');
+      setbtnClass('text-dark');
+      setBtnText('Enable Dark Mode');
+      document.body.style.backgroundColor = "white";
+      showAlert("Dark Mode has been disabled", "success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <Navbar title="TextUtils" mode={Mode} toggleMode={toggleMode} btnText={btnText} btnClass={btnClass} />
+    <Alert alertMessage={alert} />
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyze below" showAlert={showAlert} mode={Mode} toggleMode={toggleMode} btnClass={btnClass}/>
+      {/* <Routes> */}
+        {/* <Route exact path="/" element={}> */}
+        {/* </Route> */}
+        {/* <Route exact path="about" element={<About />}> */}
+        {/* </Route>
+      </Routes>       */}
     </div>
+
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to Uppercase", "success");
+    }
+    const handleLoClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+        props.showAlert("Converted to Lowercase", "success");
+    }
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+    const [text, setText] = useState(""); 
+    return (
+        <>
+        <div className={`container ${props.btnClass} p-3`}>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+              <textarea className={`form-control my-3 shadow-none bg-${props.mode} ${props.btnClass}`} onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
+            </div>          
+            <button className="btn btn-outline-primary shadow-none" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-outline-primary shadow-none ms-2" onClick={handleLoClick}>Convert to Lowecase</button>
+        </div>
+        <div className={`container mt-3 ${props.btnClass} p-3`}>
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes to read</p>
+            <h3>Preview</h3>
+            <p>{text.length > 0 ? text : "Enter Something in the above textbox to preview it"}</p>
+        </div>
+        </>
+)
+}
